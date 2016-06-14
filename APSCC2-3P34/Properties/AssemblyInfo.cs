9eee9f97ddbad57ed238b6e4d3ee3807a9815ca0
Anderson N. Silva <hegemn0@hegemn0.com>
2016-06-14 12:03:41 -0400

    Mudanças em AssemblyInfo.cs

--- APSCC2-3P34/Properties/AssemblyInfo.cs
+++ APSCC2-3P34/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("APSCC2-3P34")]
+[assembly: AssemblyTitle("Ordenador")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("APSCC2-3P34")]
+[assembly: AssemblyProduct("Ordenador")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
